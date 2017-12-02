5ebbca2091f8dfed6501199aff51fb0afaf5b16d
Nikita <toltenos@gmail.com>
2017-12-02 12:36:14 +0300

    Update init script and gitignore for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,9 +6,11 @@
  '(custom-safe-themes
    (quote
     ("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" "e1498b2416922aa561076edc5c9b0ad7b34d8ff849f335c13364c8f4276904f0" default)))
+ '(git-gutter:hide-gutter t)
+ '(git-gutter:update-interval 0.2)
  '(package-selected-packages
    (quote
-    (smex helm-descbinds neotree emacs-neotree helm-projectile helm-ag helm-config evil-escape base16-theme helm use-package markdown-mode evil-visual-mark-mode))))
+    (zenburn-theme zenburn git-gutter magit flycheck elpy material-theme smex helm-descbinds neotree emacs-neotree helm-projectile helm-ag helm-config evil-escape base16-theme helm use-package markdown-mode evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,26 +33,40 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Reload config
+;; Reload user configuration
 (defun reload-init-file ()
   (interactive)
   (load-file "~/.emacs.d/init.el"))
 
-;; Open config.
+;; Open user configuration
 (defun open-init-file ()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
-;; Colors config.
-(load-theme 'base16-chalk)
+
 ;; Style config
-(toggle-scroll-bar -1) 
-(tool-bar-mode -1)
-(menu-bar-mode -1) 
-(set-window-fringes nil 0 0)
-(set-face-attribute 'vertical-border
-                    nil
-                    :foreground "#282a2e")
+(use-package zenburn-theme
+  :ensure t
+  :config
+    (load-theme 'zenburn t)
+
+    (toggle-scroll-bar -1) 
+    (tool-bar-mode -1)
+    (menu-bar-mode -1) 
+    (set-window-fringes nil 0 0)
+    (set-face-attribute 'vertical-border
+			nil
+			:foreground "#282a2e")
+    (setq inhibit-startup-message t) ;; hide the startup message
+
+  )
+
+;; System setup
+(setq vc-follow-symlinks t)
+
+;; Show line numbers
+(global-linum-mode 1)
+(setq linum-format "%4d  ")
 
 ;;---------------------------------------- 
 ;; Neotree
@@ -60,6 +76,21 @@
   :config
     ;; Release C-e for neotree
     (define-key global-map (kbd "C-e") #'neotree-toggle)
+    (setq projectile-switch-project-action 'neotree-projectile-action)
+    (add-hook 'neotree-mode-hook
+	(lambda ()
+	(define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+	(define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
+	(define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
+	(define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
+	(define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
+	(define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
+	(define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
+
+	(define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
+	(define-key evil-normal-state-local-map (kbd "S") 'neotree-enter-horizontal-split)
+
+	(define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
   )
 
 ;;---------------------------------------- 
@@ -71,11 +102,14 @@
   (require 'helm-config)
   (setq helm-split-window-in-side-p t)
   (setq helm-autoresize-max-height 0)
-  (setq helm-autoresize-min-height 20)
+  (setq helm-autoresize-min-height 40)
   (helm-mode 1)
   (helm-autoresize-mode 1)
 
   (define-key global-map (kbd "C-p") #'helm-find)
+  (evil-leader/set-key
+    "a" 'helm-ag
+    "b" 'helm-buffers-list)
   )
 
 (use-package helm-ag
@@ -84,13 +118,6 @@
     (setq-default helm-ag-fuzzy-match 1)
   )
 
-(use-package helm-projectile
-  :ensure t)
-
-(use-package helm-descbinds
-  :ensure t
-  :config
-    (helm-descbinds-mode 1))
 
 ;;---------------------------------------- 
 ;; Smex
@@ -98,7 +125,19 @@
 (use-package smex
   :ensure t
   :config
+    (evil-leader/set-key
+	"c" 'smex)
+  
     (smex-initialize))
+
+;;----------------------------------------  
+;; Projectile
+;;---------------------------------------- 
+(use-package projectile
+  :ensure t
+  :defer t
+  :config
+    (projectile-global-mode))
 
 
 ;;---------------------------------------- 
@@ -121,8 +160,6 @@
       (interactive)
       (evil-delete (point-at-bol) (point))))
 
-  ;; Show line numbers
-  (linum-mode 1)
 
   ;; Unbind key for neotree
   (define-key evil-motion-state-map (kbd "C-e") nil)
@@ -136,10 +173,7 @@
     :config
     (evil-leader/set-key
     "<SPC>" 'evil-visual-line
-    "a" 'helm-ag
-    "b" 'helm-buffers-list
     "b" 'switch-to-buffer
-    "c" 'smex
     "e" 'find-file
     "gev" 'open-init-file
     "gsv" 'reload-init-file
@@ -174,3 +208,47 @@
   ;; TODO
   (use-package evil-indent-textobject
     :ensure t))
+
+
+
+;;---------------------------------------- 
+;; Python setup
+;;---------------------------------------- 
+(use-package elpy
+  :ensure t
+  :config
+    (elpy-use-ipython)
+    (elpy-enable))
+
+
+;;---------------------------------------- 
+;; Checkers setup
+;;---------------------------------------- 
+(use-package flycheck
+  :ensure t
+  :config
+    (when (require 'flycheck nil t)
+    (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+    (add-hook 'elpy-mode-hook 'flycheck-mode)))
+
+;;---------------------------------------- 
+;; Git integration
+;;---------------------------------------- 
+;; TODO set key bindings to move between hunks
+;; TODO set changed lines count in status bar
+(use-package magit
+  :ensure t)
+
+(use-package git-gutter
+  :ensure t
+  :config
+    (global-git-gutter-mode +1)
+    (git-gutter:linum-setup)
+
+    ;; Hide gitgutter in case of no updates
+    (custom-set-variables
+	'(git-gutter:hide-gutter t))
+
+    ;; Set update interval
+    (custom-set-variables
+	'(git-gutter:update-interval 0.2))
