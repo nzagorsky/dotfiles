21f307595faa0685aa8c6b409a2b08372f78c58a
Nikita <toltenos@gmail.com>
2017-12-03 21:31:17 +0300

    Update installation of markdown

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -235,6 +235,7 @@
 ;; Markdown
 ;;----------------------------------------
 (use-package markdown-mode
+  :ensure t
   :mode (("\\`README\\.md\\'" . gfm-mode)
          ("\\.md\\'"          . markdown-mode)
          ("\\.markdown\\'"    . markdown-mode)))
