4e04cb46ca783b1f8a4415d972e75a9e212191b9
Nikita <toltenos@gmail.com>
2018-07-15 02:49:56 +0300

    Refactor zsh settings for modularity

---
+++ .zsh/init/keyboard.zsh
@@ -0,0 +1,15 @@
+#------------------------
+# Key bindings
+#------------------------
+bindkey -e
+
+# Search through history
+bindkey "^P" up-line-or-beginning-search
+bindkey "^N" down-line-or-beginning-search
+
+# Do not display modes for previously accepted lines
+setopt transient_rprompt
+
+# Ignore command duplicates.
+setopt histignoredups
+
---
+++ .zsh/init/plugins.zsh
@@ -0,0 +1,27 @@
+source ~/.zsh/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle command-not-found
+antigen bundle compleat
+antigen bundle docker
+antigen bundle kubectl
+antigen bundle git-extras
+antigen bundle gitfast
+antigen bundle pip
+antigen bundle python
+antigen bundle z
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
+
+# Pure prompt
+antigen bundle mafredri/zsh-async
+antigen bundle sindresorhus/pure
+
+# Autsuggest
+antigen bundle zsh-users/zsh-history-substring-search
+
+# Execute
+antigen apply
