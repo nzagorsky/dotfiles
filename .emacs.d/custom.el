95119e599a48f028e9ca36cfd11c6d6b8c80e2e5
Nikita <toltenos@gmail.com>
2019-01-02 04:40:29 +0300

    Update emacs config for lsp mode with autocompletion and include custom file

---
+++ .emacs.d/custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:hide-gutter t)
+ '(git-gutter:lighter " GG")
+ '(git-gutter:modified-sign "~")
+ '(git-gutter:update-interval 0.2)
+ '(package-selected-packages
+   (quote
+    (company-lsp lsp-ui lsp-mode docker gruvbox-theme gruvbox-dark-hard-theme gruvbox-dark-hard zenburn-theme zenburn docker-compose-mode dockerfile-mode git-gutter evil-magit company navigate vimish-fold evil-indent-textobject evil-surround evil-escape evil-leader evil projectile counsel ivy smartparens esup use-package clues-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
