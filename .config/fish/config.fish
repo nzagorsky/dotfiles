12c152cfa05043ceb8405781e047411e2dd39f50
Nikita Zagorsky <toltenos@gmail.com>
2019-01-07 03:06:28 +0300

    Cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,8 +39,8 @@ set -x PATH $PATH $NIMPATH/bin
 set -x PATH $PATH $HOME/.nimble/bin  # NIM
 
 # Utils
-set -x PATH $PATH $HOME/.scripts  
-set -x PATH $PATH $HOME/.bin 
+set -x PATH $PATH $HOME/.scripts
+set -x PATH $PATH $HOME/.bin
 
 # Go
 set -x GOPATH $HOME/.go
@@ -48,61 +48,56 @@ set -x PATH $PATH $GOPATH/bin
 
 
 # FZF config
-set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude=.git || find * -type f'
+set -x FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude=.git; or find * -type f'
 set -x FZF_DEFAULT_OPTS "--inline-info"
 # }}}
 # Functions {{{
-# ex () {
-#   if [ -f $argv[1] ] ; then
-#   case $argv[1] in
-#     *.tar.bz2)   tar xjf $argv[1]                                ;;
-#     *.tar.gz)    tar xzf $argv[1]                                ;;
-#     *.bz2)       bunzip2 $argv[1]                                ;;
-#     *.rar)       unrar x $argv[1]                                ;;
-#     *.gz)        gunzip $argv[1]                                 ;;
-#     *.tar)       tar xf $argv[1]                                 ;;
-#     *.tbz2)      tar xjf $argv[1]                                ;;
-#     *.tgz)       tar xzf $argv[1]                                ;;
-#     *.zip)       unzip $argv[1]                                  ;;
-#     *.Z)         uncompress $argv[1]                             ;;
-#     *.7z)        7z x $argv[1]                                   ;;
-#     *)           echo "'$argv[1]' cannot be extracted via ex()"  ;;
-#   esac
-#   else
-#   echo "'$argv[1]' is not a valid file"
-#   fi
-# }
-
-# pk () {
-#   if [ $argv[1] ] ; then
-#   case $argv[1] in
-#     tbz)       tar cjvf $argv[2].tar.bz2 $argv[2]            ;;
-#     tgz)       tar czvf $argv[2].tar.gz  $argv[2]            ;;
-#     tar)       tar cpvf $argv[2].tar  $argv[2]               ;;
-#     bz2)       bzip $argv[2]                                 ;;
-#     gz)        gzip -c -9 -n $argv[2] > $argv[2].gz          ;;
-#     zip)       zip -r $argv[2].zip $argv[2]                  ;;
-#     7z)        7z a $argv[2].7z $argv[2]                     ;;
-#     *)         echo "'$argv[1]' cannot be packed via pk()"   ;;
-#   esac
-#   else
-#   echo "'$argv[1]' is not a valid file"
-#   fi
-# }   
-
-function fish_greeting
+function ex
+    if set -q argv[1]
+        switch $argv[1]
+        case '*.tar.bz2';   tar xjf $argv[1]                                
+        case '*.tar.gz';    tar xzf $argv[1]                                
+        case '*.bz2';       bunzip2 $argv[1]                                
+        case '*.rar';       unrar x $argv[1]                                
+        case '*.gz';        gunzip $argv[1]                                 
+        case '*.tar';       tar xf $argv[1]                                 
+        case '*.tbz2';      tar xjf $argv[1]                                
+        case '*.tgz';       tar xzf $argv[1]                                
+        case '*.zip';       unzip $argv[1]                                  
+        case '*.Z';         uncompress $argv[1]                             
+        case '*.7z';        7z x $argv[1]                                   
+        case '*';           echo "'$argv[1]' cannot be extracted via ex()"  
+    end
+else
+    echo "You no unpack this boi"
+end
 end
 
-function dutop 
+function pk
+    if set -q argv[1]
+        switch $argv[1]
+        case tbz;       tar cjvf $argv[2].tar.bz2 $argv[2]            
+        case tgz;       tar czvf $argv[2].tar.gz  $argv[2]            
+        case tar;       tar cpvf $argv[2].tar  $argv[2]               
+        case bz2;       bzip $argv[2]                                 
+        case gz;        gzip -c -9 -n $argv[2] > $argv[2].gz          
+        case zip;       zip -r $argv[2].zip $argv[2]                  
+        case 7z;        7z a $argv[2].7z $argv[2]                     
+        case *;         echo "'$argv[1]' cannot be packed via pk()"   
+    end
+else
+    echo "You no pack this boi"
+end
+end
+
+function dutop
     du --one-file-system --max-depth=2 -h $argv[1] | sort -hr | head -20;
 end
 
 # Tmux
 function t
-    if $argv[1]
-    then
+    if set -q $argv[1]
         tmux -u attach -t default; or tmux -u new -s default
-
     else
         tmux -u attach -t $argv[1]; or tmux -u new -s $argv[1]
     end
@@ -111,14 +106,14 @@ end
 function ctop
     docker run \
         --rm -it --name=ctop_(date +%s) \
-        -v /var/run/docker.sock:/var/run/docker.sock quay.io/vektorlab/ctop:latest 
+        -v /var/run/docker.sock:/var/run/docker.sock quay.io/vektorlab/ctop:latest
 end
 
 function torify
     proxychains -f ~/.config/proxychains.conf $argv
 end
 
-function v 
+function v
     if test -e Session.vim
         nvim $argv -S
     else
