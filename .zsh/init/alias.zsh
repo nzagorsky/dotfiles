eed2c7030ef4834cc9100a13b89f1fbac0c72b88
Nikita Zagorsky <toltenos@gmail.com>
2020-01-03 04:52:53 +0300

    Cleanup

--- .zsh/init/alias.zsh
+++ .zsh/init/alias.zsh
@@ -3,23 +3,14 @@ alias cya='systemctl suspend'
 alias df='df -h'                          # human-readable sizes
 alias free='free -m'                      # show sizes in MB
 alias grep='grep --color=tty -d skip'
-alias ipac='sudo pacman -S'
 alias la='ls -la --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias ll='ls -lhs --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias lla='ls -lahs --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias p='ipython'
 alias rm='rm -I'
-alias spac='pacman -Ss'
-alias upac='sudo pacman -Syu'
 alias e='emacs -nw'
-alias k='kubectl'
-alias r='ranger'
 alias g=git
 alias v=nvim
 
-alias ping='grc --colour=auto ping'
-alias diff='grc --colour=auto diff'
-
-alias scw='docker run -it --rm --volume=$HOME/.scwrc:/.scwrc scaleway/cli'
 alias getmirrors='sudo pacman-mirrors -c Russia,Belarus,Ukraine,Poland,Netherlands'
