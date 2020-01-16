14d4a7ae6da7e2746c1aa8582e0f64530fa9fbb1
Nikita Zagorsky <toltenos@gmail.com>
2020-01-16 07:37:58 +0300

    Few updates

--- .zsh/init/plugins.zsh
+++ .zsh/init/plugins.zsh
@@ -2,25 +2,28 @@ source ~/.zsh/antigen/antigen.zsh
 
 antigen use oh-my-zsh
 
-antigen bundle command-not-found
+antigen bundle aws
 antigen bundle compleat
 antigen bundle docker
-antigen bundle kubectl
 antigen bundle git-extras
 antigen bundle gitfast
+antigen bundle httpie
+antigen bundle kubectl
+antigen bundle npm
 antigen bundle pip
 antigen bundle python
+antigen bundle redis-cli
+antigen bundle yarn
 antigen bundle z
 
-# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-autosuggestions
+antigen bundle zsh-users/zsh-completions
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-history-substring-search
 
 # Pure prompt
 antigen bundle mafredri/zsh-async
 antigen bundle sindresorhus/pure
-
-# Autsuggest
-antigen bundle zsh-users/zsh-history-substring-search
 
 # Execute
 antigen apply
