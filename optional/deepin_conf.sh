3dcae1ee182ddbba7d3f81045731f29a6b2b5edd
Nikita Zagorsky <toltenos@gmail.com>
2020-03-07 06:27:44 +0300

    Remove deepin setup script

--- optional/deepin_conf.sh
+++
@@ -1,4 +0,0 @@
-# TODO dump dconf after all settings and just reproduce it as a script. Voila.
-# Set of scripts and commands to setup deepin properly.
-gsettings set com.deepin.wrap.gnome.desktop.wm.preferences mouse-button-modifier '<Alt>'
-gsettings set com.deepin.dde.appearance gtk-theme 'deepin-dark'
