853c4f3d782a35775ab91971f5d016fc139d0154
Nikita Zagorsky <toltenos@gmail.com>
2020-11-03 01:04:23 +0300

    update coc settings

--- src/emacs/.doom.d/custom.el
+++ src/emacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" default)))
+   '("e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
