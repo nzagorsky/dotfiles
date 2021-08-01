1e3b2e2013edaf784cf728b64a884863e15f308f
Nikita Zagorsky <toltenos@gmail.com>
2021-08-01 23:17:34 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ set -e
 GNOME_EXTENSION_INSTALLER=~/.scripts/gnome-shell-extension-installer
 
 setup_yay() {
-    pacman -S --needed git base-devel
+    sudo pacman -S --needed git base-devel
     git clone https://aur.archlinux.org/yay.git
     cd yay
-    makepkg -si
+    sudo makepkg -si
 }
 
 install_npm_deps () {
