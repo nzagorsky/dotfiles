3f445341495458165055b91cc2e2c1cd547725dc
Nikita Zagorsky <toltenos@gmail.com>
2023-04-16 16:50:56 -0400

    dev: support more languages in ctags

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -195,22 +195,6 @@ require("lazy").setup {
             vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
         end,
     },
-
-    "nvim-lua/plenary.nvim",
-
-    -- {
-    --     "folke/which-key.nvim",
-    --     config = function()
-    --         vim.o.timeout = true
-    --         vim.o.timeoutlen = 300
-    --         require("which-key").setup {
-    --             -- your configuration comes here
-    --             -- or leave it empty to use the default settings
-    --             -- refer to the configuration section below
-    --         }
-    --     end,
-    -- },
-
     {
         "nvim-telescope/telescope.nvim",
         dependencies = {
