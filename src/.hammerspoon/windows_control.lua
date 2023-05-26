d6bab36561e3352be96024b088bfc6c48bd47c58
Nikita Zagorsky <toltenos@gmail.com>
2023-05-25 23:20:42 -0400

    fix: restore units back

--- src/.hammerspoon/windows_control.lua
+++ src/.hammerspoon/windows_control.lua
@@ -25,6 +25,26 @@ module.MoveWindowToSpace = function(sp)
     spaces.moveWindowToSpace(win:id(), spaceID)
 end
 
+module.units = {
+    right30 = { x = 0.70, y = 0.00, w = 0.30, h = 1.00 },
+    right50 = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
+    right70 = { x = 0.30, y = 0.00, w = 0.70, h = 1.00 },
+    left30 = { x = 0.00, y = 0.00, w = 0.30, h = 1.00 },
+    left50 = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
+    left70 = { x = 0.00, y = 0.00, w = 0.70, h = 1.00 },
+    top50 = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
+    bot50 = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+    bot80 = { x = 0.00, y = 0.20, w = 1.00, h = 0.80 },
+    bot87 = { x = 0.00, y = 0.20, w = 1.00, h = 0.87 },
+    bot90 = { x = 0.00, y = 0.20, w = 1.00, h = 0.90 },
+    upright30 = { x = 0.70, y = 0.00, w = 0.30, h = 0.50 },
+    botright30 = { x = 0.70, y = 0.50, w = 0.30, h = 0.50 },
+    upleft70 = { x = 0.00, y = 0.00, w = 0.70, h = 0.50 },
+    botleft70 = { x = 0.00, y = 0.50, w = 0.70, h = 0.50 },
+    right70top80 = { x = 0.70, y = 0.00, w = 0.30, h = 0.80 },
+    maximum = { x = 0.00, y = 0.00, w = 1.00, h = 1.00 },
+    center = { x = 0.20, y = 0.10, w = 0.60, h = 0.80 },
+}
 
 -- Shortcuts
 hs.hotkey.bind({ "alt", "shift" }, tostring(0), function() module.MoveWindowToSpace(10) end)
