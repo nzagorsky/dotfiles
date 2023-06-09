ed0636c7ed6c0b05e2e9f388069716ce84c9881a
Nikita Zagorsky <toltenos@gmail.com>
2023-06-09 01:47:49 -0400

    dev: add snippets support and speed up cmp

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -243,6 +243,39 @@ require("lazy").setup {
         config = require("plugins.configs.cmpconf").config,
         dependencies = {
             {
+                -- snippet plugin
+                "L3MON4D3/LuaSnip",
+                dependencies = "rafamadriz/friendly-snippets",
+                opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+                config = function(_, opts)
+                    require("luasnip").config.set_config(opts)
+
+                    -- vscode format
+                    require("luasnip.loaders.from_vscode").lazy_load()
+                    require("luasnip.loaders.from_vscode").lazy_load { paths = vim.g.vscode_snippets_path or "" }
+
+                    -- snipmate format
+                    require("luasnip.loaders.from_snipmate").load()
+                    require("luasnip.loaders.from_snipmate").lazy_load { paths = vim.g.snipmate_snippets_path or "" }
+
+                    -- lua format
+                    require("luasnip.loaders.from_lua").load()
+                    require("luasnip.loaders.from_lua").lazy_load { paths = vim.g.lua_snippets_path or "" }
+
+                    vim.api.nvim_create_autocmd("InsertLeave", {
+                        callback = function()
+                            if
+                                require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                                and not require("luasnip").session.jump_active
+                            then
+                                require("luasnip").unlink_current()
+                            end
+                        end,
+                    })
+                end,
+            },
+
+            {
                 "windwp/nvim-autopairs",
                 opts = {
                     fast_wrap = {},
@@ -256,6 +289,7 @@ require("lazy").setup {
                     require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
                 end,
             },
+            "saadparwaiz1/cmp_luasnip",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-nvim-lua",
@@ -411,6 +445,7 @@ require("lazy").setup {
 
     {
         "tpope/vim-fugitive",
+        event = { "InsertEnter", "CmdlineEnter" },
         keys = {
             { "<leader>gs", "<cmd>Git<cr>" },
             { "<leader>gc", "<cmd>Git commit<cr>" },
