f9e799db36aeb6ebb291c8bfae2723aae9aa7498
Nikita Zagorsky <toltenos@gmail.com>
2023-12-08 23:41:13 -0500

    update hotkeys for wezterm

--- src/.config/wezterm/wezterm.lua
+++ src/.config/wezterm/wezterm.lua
@@ -23,7 +23,7 @@ for i = 0, 9, 1 do
 	})
 end
 
-local hotkeys = { "f", "b", "h", "j", "k", "l", "p", "w" }
+local hotkeys = { "f", "b", "h", "j", "k", "l", "p"}
 
 for _, v in pairs(hotkeys) do
 	print(v)
