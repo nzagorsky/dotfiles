57937566f86f360fae37344cb5f9e9353c29f978
Nikita Zagorsky <toltenos@gmail.com>
2024-02-23 00:52:51 -0500

    dev: convenience changes

--- src/.config/nvim/lua/plugins/configs/cmpconf.lua
+++ src/.config/nvim/lua/plugins/configs/cmpconf.lua
@@ -9,7 +9,7 @@ local M = {
             return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
         end
 
-        local max_item_count = 20
+        local max_item_count = 5
 
         local buffer_words_source = {
             name = "buffer",
