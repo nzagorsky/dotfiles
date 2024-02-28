033bdf3841f6391c9215da8fdf77dcae68a7b1d5
Nikita Zagorsky <toltenos@gmail.com>
2024-02-28 00:42:22 -0500

    chore: a bit of cmp refactor

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -94,7 +94,23 @@ require("lazy").setup {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
-        config = function() vim.cmd "colorscheme catppuccin-mocha" end,
+        config = function()
+            require("catppuccin").setup {
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+                },
+            }
+            vim.cmd "colorscheme catppuccin-mocha"
+        end,
     },
 
     {
