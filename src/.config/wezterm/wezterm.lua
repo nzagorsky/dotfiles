5b90b5e89c1ca50df121fc5fcc2c6b2cca31ee36
Nikita Zagorsky <toltenos@gmail.com>
2024-02-28 01:09:29 -0500

    dev: reduce font size

--- src/.config/wezterm/wezterm.lua
+++ src/.config/wezterm/wezterm.lua
@@ -5,10 +5,12 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14.0
+
 config.color_scheme = "catppuccin-mocha"
 config.hide_tab_bar_if_only_one_tab = true
--- config.font = wezterm.font("JetBrains Mono")
-config.font_size = 15.0
+
 config.check_for_updates = false
 config.show_update_window = false
 config.animation_fps = 1
