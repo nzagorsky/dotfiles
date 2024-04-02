72ccf0525b75c628b36d9e8082b7f4679e2072d5
Nikita Zagorsky <toltenos@gmail.com>
2024-04-02 15:35:05 -0400

    chore: minor cleanup

--- src/.config/nvim/lua/plugins/configs/cmpconf.lua
+++ src/.config/nvim/lua/plugins/configs/cmpconf.lua
@@ -25,6 +25,7 @@ local M = {
             snippet = {
                 expand = function(args) require("luasnip").lsp_expand(args.body) end,
             },
+
             mapping = cmp.mapping.preset.insert {
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
