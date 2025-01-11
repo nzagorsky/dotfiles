52c1e5c99f0489c1f405f0110fe3adb6676853d4
Nikita Zagorsky <nikita@zagorsky.dev>
2025-01-11 01:39:25 -0500

    chore: cleanup

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -210,36 +210,6 @@ local plugins = {
         opts_extend = { "sources.default" },
     },
 
-    -- {
-    --     "hrsh7th/nvim-cmp",
-    --     config = require("plugins.configs.cmpconf").config,
-    --     dependencies = {
-    --         { "L3MON4D3/LuaSnip" },
-    --
-    --         {
-    --             "windwp/nvim-autopairs",
-    --             opts = {
-    --                 fast_wrap = {},
-    --                 disable_filetype = { "vim" },
-    --             },
-    --
-    --             config = function(_, opts)
-    --                 require("nvim-autopairs").setup(opts)
-    --
-    --                 local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-    --                 require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    --             end,
-    --         },
-    --
-    --         "saadparwaiz1/cmp_luasnip",
-    --         "hrsh7th/cmp-nvim-lsp",
-    --         "hrsh7th/cmp-buffer",
-    --         "hrsh7th/cmp-nvim-lua",
-    --         "hrsh7th/cmp-path",
-    --         "hrsh7th/cmp-cmdline",
-    --     },
-    -- },
-
     {
         "nvim-treesitter/nvim-treesitter",
         config = function()
