593ca3c621eeeb033f4f0b4e4aebd6799516b67f
Nikita Zagorskii <NIKITA.ZAGORSKII1@accesscfa.com>
2025-06-26 12:39:06 -0400

    dev: simplify catpuccin setup and return neovim transparency

--- src/.config/nvim/init.lua
+++ src/.config/nvim/init.lua
@@ -13,14 +13,14 @@ local plugins = {
         config = function()
             require("catppuccin").setup {
                 flavour = "mocha", -- latte, frappe, macchiato, mocha
-                -- transparent_background = true, -- disables setting the background color.
-                -- color_overrides = {
-                --     mocha = {
-                --         base = "#181818",
-                --         crust = "#111111",
-                --         mantle = "#161616",
-                --     },
-                -- },
+                transparent_background = true, -- disables setting the background color.
+                color_overrides = {
+                    mocha = {
+                        base = "#181818",
+                        crust = "#111111",
+                        mantle = "#161616",
+                    },
+                },
             }
             vim.cmd.colorscheme "catppuccin-mocha"
         end,
