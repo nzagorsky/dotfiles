7b5140eda37163177a2c40a18c1de326c326ce6b
Nikita Zagorskii <NIKITA.ZAGORSKII1@accesscfa.com>
2025-07-29 11:57:34 -0400

    dev: minor updates

--- src/.config/nvim/lua/plugins/blink.cmp.lua
+++ src/.config/nvim/lua/plugins/blink.cmp.lua
@@ -3,6 +3,17 @@ return {
         "saghen/blink.cmp",
         dependencies = {
             { "rafamadriz/friendly-snippets" },
+            { "saghen/blink.compat" },
+            {
+                "supermaven-inc/supermaven-nvim",
+                opts = {
+                    keymaps = {
+                        accept_suggestion = nil,
+                    },
+                    disable_inline_completion = true,
+                    ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
+                },
+            },
         },
         event = { "InsertEnter", "CmdlineEnter" },
         version = "1.*",
@@ -11,18 +22,29 @@ return {
             sources = {
                 default = {
                     "lazydev",
+                    "supermaven",
                     "lsp",
                     "path",
                     "snippets",
+                    "buffer",
                 },
                 providers = {
-                    lsp = {
-                        score_offset = 10,
-                    },
                     lazydev = {
                         name = "LazyDev",
                         module = "lazydev.integrations.blink",
                         score_offset = 100,
+                    },
+                    lsp = {
+                        score_offset = 10,
+                    },
+                    buffer = {
+                        score_offset = -10,
+                    },
+                    supermaven = {
+                        name = "supermaven",
+                        module = "blink.compat.source",
+                        score_offset = 100,
+                        async = true,
                     },
                 },
             },
--- src/.config/nvim/lua/plugins/nvim-treesitter-context.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "nvim-treesitter/nvim-treesitter-context",
-    event = "InsertEnter",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    config = function() require("treesitter-context").setup() end,
-}
