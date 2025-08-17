859c9cd9d51746b1b8e9d9b9de22271ade5d7ff9
Nikita Zagorsky <nikita@zagorsky.dev>
2025-08-17 12:22:32 +0300

    dev: load treesitter on cmd as well

--- src/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ src/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     event = { "BufRead", "BufNewFile" },
+    cmd = { "TSUpdateSync" },
     -- branch = "main",
     build = ":TSUpdate",
     config = function()
