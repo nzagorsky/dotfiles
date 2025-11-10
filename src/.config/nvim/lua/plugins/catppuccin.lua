81b56e9dde903a9f84334c0e2a52c1e2cb05885d
Nikita Zagorsky <nikita@zagorsky.dev>
2025-11-10 18:48:39 -0500

    chore: cleanup

--- src/.config/nvim/lua/plugins/catppuccin.lua
+++ src/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,16 +4,16 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup {
-            flavour = "latte", -- latte, frappe, macchiato, mocha
+            flavour = "mocha", -- latte, frappe, macchiato, mocha
             transparent_background = true,
-            -- color_overrides = {
-            --     mocha = {
-            --         base = "#181818",
-            --         crust = "#111111",
-            --         mantle = "#161616",
-            --     },
-            -- },
+            color_overrides = {
+                mocha = {
+                    base = "#181818",
+                    crust = "#111111",
+                    mantle = "#161616",
+                },
+            },
         }
-        vim.cmd.colorscheme "catppuccin-latte"
+        vim.cmd.colorscheme "catppuccin-mocha"
     end,
 }
