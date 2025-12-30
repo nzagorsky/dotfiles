da7e04adbadbb5d218f6c48d083f4e177fa526e3
Nikita Zagorsky <nikita@zagorsky.dev>
2025-12-30 18:45:08 -0500

    dev: leave alacritty

--- src/.hammerspoon/app_launcher.lua
+++ src/.hammerspoon/app_launcher.lua
@@ -1,3 +1,3 @@
 -- hs.hotkey.bind("alt", "return", function() hs.execute "open -n /Applications/Alacritty.app" end)
-hs.hotkey.bind("alt", "return", function() hs.execute("open -n /Applications/Alacritty.app", true) end)
+hs.hotkey.bind("alt", "return", function() hs.execute("open -n /Applications/Ghostty.app", true) end)
 hs.hotkey.bind("alt", "e", function() hs.execute("open ~", true) end)
