cc62f804e6d1a1b76bd1c7c1baa8622b569cd000
Nikita Zagorsky <nikita@zagorsky.dev>
2026-01-22 11:47:25 -0500

    dev: update install.sh script

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
+
+require_cmd() {
+    command -v "$1" >/dev/null 2>&1 || {
+        echo "Missing required command: $1" >&2
+        exit 1
+    }
+}
+
+clone_or_update() {
+    local repo="$1"
+    local dest="$2"
+    local depth="${3:-1}"
+
+    if [[ -d "$dest/.git" ]]; then
+        git -C "$dest" fetch --depth "$depth" origin
+        git -C "$dest" reset --hard "origin/$(git -C "$dest" rev-parse --abbrev-ref HEAD)"
+        return 0
+    fi
+
+    rm -rf "$dest"
+    git clone --depth "$depth" "$repo" "$dest"
+}
 
 pull_submodules() {
     git submodule update --init --recursive --depth 1
-    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+    clone_or_update https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 }
 
 setup_dots() {
@@ -16,26 +38,23 @@ setup_dots() {
 }
 
 setup_shell_zsh() {
-    curl -s https://raw.githubusercontent.com/agkozak/polyglot/master/polyglot.sh >~/.local/bin/polyglot.sh
+    curl -fSL https://raw.githubusercontent.com/agkozak/polyglot/master/polyglot.sh >~/.local/bin/polyglot.sh
     touch ~/.config/wget/wgetrc
 
-    rm -rf ~/.config/zsh/plugins/zsh-syntax-highlighting/
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
-
-    rm -rf ~/.config/zsh/plugins/zsh-defer/
-    git clone https://github.com/romkatv/zsh-defer.git ~/.config/zsh/plugins/zsh-defer/
-
-    rm -rf ~/.config/zsh/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
-
-    rm -rf ~/.config/zsh/plugins/zsh-autocomplete
-    git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.config/zsh/plugins/zsh-autocomplete
+    clone_or_update https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/plugins/zsh-syntax-highlighting
+    clone_or_update https://github.com/romkatv/zsh-defer.git ~/.config/zsh/plugins/zsh-defer
+    clone_or_update https://github.com/zsh-users/zsh-autosuggestions ~/.config/zsh/plugins/zsh-autosuggestions
+    clone_or_update https://github.com/marlonrichert/zsh-autocomplete.git ~/.config/zsh/plugins/zsh-autocomplete
 }
 
 setup_completions() {
     mkdir -p $HOME/.config/zsh/completion
-    curl -L https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker >$HOME/.config/zsh/completion/_docker
+    curl -fSL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker >$HOME/.config/zsh/completion/_docker
 }
+
+require_cmd git
+require_cmd curl
+require_cmd stow
 
 pull_submodules || echo "Failed to pull submodules"
 setup_dots
